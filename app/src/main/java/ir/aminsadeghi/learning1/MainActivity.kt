1f750c1c16e36a2bae1648e90169dd9d78b07843
Amin <ce.aminsadeghi@gmail.com>
2020-03-20 09:28:48 +0330

    changes tv1 value

--- app/src/main/java/ir/aminsadeghi/learning1/MainActivity.kt
+++ app/src/main/java/ir/aminsadeghi/learning1/MainActivity.kt
@@ -2,11 +2,13 @@ package ir.aminsadeghi.learning1
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        tv1.text="Hello Github!"
     }
 }
